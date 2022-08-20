6c8d5d58d11b1871442793488657a49ed683e9d4
wumian <1285837374@qq.com>
2022-08-20 22:58:25 +0800

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -80,7 +80,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           // { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
           // { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
           // { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-          // { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+          { text: 'MySQL', link: '/pages/f1315b/' },
           { text: 'ElasticSearch', link: '/pages/927cf0/'  },
         ],
       },
